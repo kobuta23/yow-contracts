838fa1deadcf49080c21bc4ca3e78aa5cab740db
stawberryjuice <annathunguyenova@gmail.com>
2024-04-16 14:25:43 +0200

    ..

--- src/YoW.sol
+++ src/YoW.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.13;
 import {ISuperfluid, ISuperToken, ISuperApp, SuperAppDefinitions} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import {ISuperfluidPool} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/gdav1/ISuperfluidPool.sol";
 import {SuperTokenV1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
-import {SuperAppBaseFlow} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBaseFlow.sol";
-import {BasicParticle} from "@superfluid-finance/solidity-semantic-money/contracts/SemanticMoney.sol";
+import {SuperAppBase} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+import {BasicParticle} from "@superfluid-finance/solidity-semantic-money/src/SemanticMoney.sol";
 
-contract YoW {
+contract YoW is SuperAppBase {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
